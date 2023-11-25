138c1a3c4ecb5059d44adc9951687e7f50695aa5
Akhil Jose <dev.akhiljoseko@gmail.com>
2023-11-25 17:38:18 +0530

    providing auth cubit in widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:life_flow/app/app.dart';
+import 'package:life_flow/core/authentication/cubit/authentication_cubit.dart';
 
 void main() {
-  runApp(const LifeFlowApp());
+  runApp(
+    BlocProvider(
+      create: (context) => AuthenticationCubit(),
+      child: const LifeFlowApp(),
+    ),
+  );
 }
