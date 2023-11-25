f7dadb76bb6b625361585c22f58a52c6a7321e40
Akhil Jose <dev.akhiljoseko@gmail.com>
2023-11-25 23:59:09 +0530

    welcome screen ui

--- lib/view/welcome/welcome_screen.dart
+++ lib/view/welcome/welcome_screen.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:life_flow/app/app.dart';
+import 'package:life_flow/view/widgets/spacing.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Welcome"),
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(AppImages.welcome_bg),
+            colorFilter: ColorFilter.mode(
+              Color(0xFFEF9390),
+              BlendMode.softLight,
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "LifeFlow",
+                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      color: AppColors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const Vspace(12),
+              Text(
+                "Connecting donors with those in need, making blood donation easier than ever",
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: AppColors.white,
+                    ),
+              ),
+              const Vspace(24),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.maxFinite, 50),
+                ),
+                child: const Text("Log In"),
+              ),
+              const Vspace(12),
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  minimumSize: const Size(double.maxFinite, 50),
+                ),
+                child: const Text("Register"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
