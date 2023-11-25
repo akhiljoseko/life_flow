9172409b6f9a742f7c42cbcfe9a589e4e03faba6
Akhil Jose <dev.akhiljoseko@gmail.com>
2023-11-25 23:58:46 +0530

    spacing widget

---
+++ lib/view/widgets/spacing.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Hspace extends StatelessWidget {
+  const Hspace(this.width, {super.key});
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+    );
+  }
+}
+
+class Vspace extends StatelessWidget {
+  const Vspace(this.height, {super.key});
+
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+    );
+  }
+}
