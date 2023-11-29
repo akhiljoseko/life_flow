117d94823a24616df0284ef950146349c92300eb
Akhil Jose <dev.akhiljoseko@gmail.com>
2023-11-29 13:16:19 +0530

    extracted text button from login to use in sign up

---
+++ lib/view/widgets/question_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class QuestionButton extends StatelessWidget {
+  const QuestionButton({
+    super.key,
+    required this.question,
+    required this.buttonText,
+    required this.onPressed,
+  });
+
+  final String question;
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: question,
+        style: Theme.of(context)
+            .textTheme
+            .bodyMedium
+            ?.copyWith(color: Colors.black),
+        children: [
+          TextSpan(
+            text: buttonText,
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+            recognizer: TapGestureRecognizer()..onTap = onPressed,
+          )
+        ],
+      ),
+    );
+  }
+}
