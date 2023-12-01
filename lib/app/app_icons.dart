dbc1923a821a0eefd2a4037bf1e8c43029b19fbe
Akhil Jose <dev.akhiljoseko@gmail.com>
2023-12-01 20:52:18 +0530

    Added new icons
    - search icon
    - filter icon
    - location icon
    - class to manage icon assets

---
+++ lib/app/app_icons.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: constant_identifier_names
+
+class AppIcons {
+  static const _location = "assets/icons/";
+
+  static const search_outlined = "${_location}search_outlined.svg";
+  static const filter_outlined = "${_location}filter_outlined.svg";
+  static const location_outlined = "${_location}location_outlined.svg";
+}
